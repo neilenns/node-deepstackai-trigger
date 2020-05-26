5e44f804236a11aadfe876a679178115aec0337b
Neil Enns <info@danecreekphotography.com>
2020-05-25 22:37:19 -0700

    Move IWebRequestHandlerJson Fixes #49

--- src/types/ITriggerJson.ts
+++ src/types/ITriggerJson.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import IMqttConfigJson from '../handlers/mqttManager/IMqttConfigJson';
-import IWebRequestHandlerJson from './IWebRequestHandlerJson';
+import IWebRequestHandlerJson from '../handlers/webRequest/IWebRequestHandlerJson';
 
 export default interface ITriggerJson {
   cooldownTime: number;
