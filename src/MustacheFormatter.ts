e3d59c99c995fbb62f5a205e558637939681d801
Neil Enns <info@danecreekphotography.com>
2020-06-09 17:16:23 -0700

    Provide detection summary to the trigger URI Fixes #148

--- src/MustacheFormatter.ts
+++ src/MustacheFormatter.ts
@@ -8,6 +8,21 @@ import Trigger from "./Trigger";
 import IDeepStackPrediction from "./types/IDeepStackPrediction";
 import path from "path";
 
+function formatPredictions(predictions: IDeepStackPrediction[]): string {
+  return predictions
+    .map(prediction => {
+      return `${prediction.label} (${(prediction.confidence * 100).toFixed(0)}%)`;
+    })
+    .join(", ");
+}
+
+/**
+ * Replaces mustache templates in a string with values
+ * @param template The template string to format
+ * @param fileName The filename of the image being analyzed
+ * @param trigger The trigger that was fired
+ * @param predictions The predictions returned by the AI system
+ */
 export function format(
   template: string,
   fileName: string,
@@ -19,6 +34,7 @@ export function format(
     fileName,
     baseName: path.basename(fileName),
     predictions: JSON.stringify(predictions),
+    formattedPredictions: formatPredictions(predictions),
     state: "on",
     name: trigger.name,
   };
@@ -28,5 +44,5 @@ export function format(
     return text;
   };
 
-  return Mustache.render(trigger.mqttConfig.payload, view);
+  return Mustache.render(template, view);
 }
