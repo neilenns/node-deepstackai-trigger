8f0ce7e8208d97140ab92b813aff23feb5f4b978
Neil Enns <info@danecreekphotography.com>
2020-06-12 20:32:09 -0700

    Draws stuff and text... but maybe not in Docker. We'll see.

--- src/handlers/annotationManager/AnnotationManager.ts
+++ src/handlers/annotationManager/AnnotationManager.ts
@@ -8,21 +8,36 @@ import IDeepStackPrediction from "../../types/IDeepStackPrediction";
 import * as LocalStorageManager from "../../LocalStorageManager";
 import PImage from "pureimage";
 import * as fs from "fs";
+// Referenced only so webpack picks it up in production Docker image builds
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import CascadiaCode from "../../CascadiaCode.ttf";
 
 export async function processTrigger(
   fileName: string,
   trigger: Trigger,
   predictions: IDeepStackPrediction[],
 ): Promise<string> {
-  const decodedImage = await PImage.decodeJPEGFromStream(fs.createReadStream(fileName));
-  const context = decodedImage.getContext("2d");
-  context.strokeStyle = "rgba(255,0,0,0.75)";
+  const outputFileName = LocalStorageManager.mapToLocalStorage(fileName);
+  const font = PImage.registerFont("./src/CascadiaCode.ttf", "Cascadia Code");
 
-  predictions.map(prediction => {
-    context.strokeRect(prediction.x_min, prediction.y_min, prediction.x_max, prediction.y_max);
+  font.load(async () => {
+    const decodedImage = await PImage.decodeJPEGFromStream(fs.createReadStream(fileName));
+    const context = decodedImage.getContext("2d");
+    context.strokeStyle = "rgba(255,0,0,0.75)";
+    context.fillStyle = "rgba(255,0,0,0.75)";
+    context.font = "18pt Cascadia Code";
+    context.fontBaseline = "top";
+
+    predictions.map(prediction => {
+      context.strokeRect(prediction.x_min, prediction.y_min, prediction.x_max, prediction.y_max);
+      context.fillText(
+        `${prediction.label} (${(prediction.confidence * 100).toFixed(0)}%)`,
+        prediction.x_min + 10,
+        prediction.y_min + 24,
+      );
+    });
+    PImage.encodeJPEGToStream(decodedImage, fs.createWriteStream(outputFileName), 75);
   });
 
-  const outputFileName = LocalStorageManager.mapToLocalStorage(fileName);
-  PImage.encodeJPEGToStream(decodedImage, fs.createWriteStream(outputFileName), 75);
   return outputFileName;
 }
