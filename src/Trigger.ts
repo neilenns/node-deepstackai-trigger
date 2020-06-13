37761a7aad6cc9623bdfcb0aec8c64335a07a297
Neil Enns <info@danecreekphotography.com>
2020-06-13 11:02:35 -0700

    Fix async with annotations

--- src/Trigger.ts
+++ src/Trigger.ts
@@ -105,13 +105,11 @@ export default class Trigger {
     // Generate the annotations so it is ready for the other trigger handlers
     await AnnotationManager.processTrigger(fileName, this, triggeredPredictions);
 
-    // Call all the handlers for the trigger
-    await Promise.all([
-      ...(await WebRequestHandler.processTrigger(fileName, this, triggeredPredictions)),
-      ...(await MqttManager.processTrigger(fileName, this, triggeredPredictions)),
-      ...(await MqttManager.publishStatisticsMessage(TriggerManager.triggeredCount, TriggerManager.analyzedFilesCount)),
-      ...(await TelegramManager.processTrigger(fileName, this, triggeredPredictions)),
-    ]);
+    // Call all the handlers for the trigger. There is no need to wait for these to finish before proceeding
+    WebRequestHandler.processTrigger(fileName, this, triggeredPredictions);
+    MqttManager.processTrigger(fileName, this, triggeredPredictions);
+    MqttManager.publishStatisticsMessage(TriggerManager.triggeredCount, TriggerManager.analyzedFilesCount);
+    TelegramManager.processTrigger(fileName, this, triggeredPredictions);
   }
 
   /**
--- src/handlers/annotationManager/AnnotationManager.ts
+++ src/handlers/annotationManager/AnnotationManager.ts
@@ -8,33 +8,35 @@ import IDeepStackPrediction from "../../types/IDeepStackPrediction";
 import * as LocalStorageManager from "../../LocalStorageManager";
 import PImage from "pureimage";
 import * as fs from "fs";
+import * as log from "../../Log";
 
 export async function processTrigger(
   fileName: string,
   trigger: Trigger,
   predictions: IDeepStackPrediction[],
-): Promise<string> {
+): Promise<void> {
+  log.info("Annotations", `Annotating ${fileName}`);
   const outputFileName = LocalStorageManager.mapToLocalStorage(fileName);
   const font = PImage.registerFont("./fonts/CascadiaCode.ttf", "Cascadia Code");
 
-  await font.load(async () => {
-    const decodedImage = await PImage.decodeJPEGFromStream(fs.createReadStream(fileName));
-    const context = decodedImage.getContext("2d");
-    context.strokeStyle = "rgba(255,0,0,0.75)";
-    context.fillStyle = "rgba(255,0,0,0.75)";
-    context.font = "18pt Cascadia Code";
-    context.fontBaseline = "top";
+  await font.load();
 
-    predictions.map(prediction => {
-      context.strokeRect(prediction.x_min, prediction.y_min, prediction.x_max, prediction.y_max);
-      context.fillText(
-        `${prediction.label} (${(prediction.confidence * 100).toFixed(0)}%)`,
-        prediction.x_min + 10,
-        prediction.y_min + 24,
-      );
-    });
-    PImage.encodeJPEGToStream(decodedImage, fs.createWriteStream(outputFileName), 75);
+  const decodedImage = await PImage.decodeJPEGFromStream(fs.createReadStream(fileName));
+  const context = decodedImage.getContext("2d");
+  context.strokeStyle = "rgba(255,0,0,0.75)";
+  context.fillStyle = "rgba(255,0,0,0.75)";
+  context.font = "18pt Cascadia Code";
+  context.fontBaseline = "top";
+
+  predictions.map(prediction => {
+    context.strokeRect(prediction.x_min, prediction.y_min, prediction.x_max, prediction.y_max);
+    context.fillText(
+      `${prediction.label} (${(prediction.confidence * 100).toFixed(0)}%)`,
+      prediction.x_min + 10,
+      prediction.y_min + 24,
+    );
   });
 
-  return outputFileName;
+  await PImage.encodeJPEGToStream(decodedImage, fs.createWriteStream(outputFileName), 75);
+  log.info("Annotations", `Done annotating ${fileName}`);
 }
