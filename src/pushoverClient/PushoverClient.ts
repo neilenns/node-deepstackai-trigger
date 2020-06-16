772301115862f47b62c0a46109878565400b60a6
Neil Enns <info@danecreekphotography.com>
2020-06-16 06:19:33 -0700

    Add doc comments and some error checking

--- src/pushoverClient/PushoverClient.ts
+++ src/pushoverClient/PushoverClient.ts
@@ -7,15 +7,36 @@ import request from "request-promise-native";
 import * as log from "../Log";
 import fs from "fs";
 
+/**
+ * A basic client for sending messages to the Pushover REST API
+ */
 export default class PushoverClient {
+  /**
+   * The api key/token registered for the application
+   */
   public apiKey: string;
+  /**
+   * The user key/token that registered the application
+   */
   public userKey: string;
 
   constructor(init?: Partial<PushoverClient>) {
     Object.assign(this, init);
   }
 
+  /**
+   * Asynchronously sends a message to the Pushover REST API.
+   * @param message The PushoverMessage to send
+   */
   public async send(message: PushoverMessage): Promise<void> {
+    if (!this.apiKey) {
+      throw Error("apiKey must be set before calling send().");
+    }
+
+    if (!this.userKey) {
+      throw Error("userKey must be set before calling send().");
+    }
+
     const form = {
       token: this.apiKey,
       user: this.userKey,
