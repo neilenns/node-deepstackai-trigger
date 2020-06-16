5188cd42798f6d93cf02afac887138f5ca33cb1a
Neil Enns <info@danecreekphotography.com>
2020-06-15 21:20:15 -0700

    My own client and settings all plumbed through

--- src/pushoverClient/PushoverMessage.ts
+++ src/pushoverClient/PushoverMessage.ts
@@ -2,4 +2,14 @@
  *  Copyright (c) Neil Enns. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-declare module "pushover-notifications";
+
+export default class PushoverMessage {
+  public message: string;
+  public userKey?: string;
+  public device?: string;
+  public imageFileName: string;
+
+  constructor(init?: Partial<PushoverMessage>) {
+    Object.assign(this, init);
+  }
+}
