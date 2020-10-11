5f78fb78d2dc80c6ee89f4128ce3f231c48a347c
Neil Enns <info@danecreekphotography.com>
2020-10-11 08:35:56 -0700

    Issue 380: Format tests

--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Neil Enns. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import {closeSync, existsSync, openSync, unlinkSync, writeFileSync} from 'fs';
+import { closeSync, existsSync, openSync, unlinkSync, writeFileSync } from "fs";
 import * as JSONC from "jsonc-parser";
 import * as helpers from "./../src/helpers";
 
@@ -10,7 +10,7 @@ describe("helpers", () => {
   const settingsFilePath = `${__dirname}/settings.json`;
 
   beforeEach(() => {
-    closeSync(openSync(settingsFilePath, 'w'));
+    closeSync(openSync(settingsFilePath, "w"));
   });
 
   afterEach(() => {
@@ -20,71 +20,77 @@ describe("helpers", () => {
     }
   });
 
-   test("Verify can load settings.json", () => {
-     const serviceName = "Settings";
-     const expectedSettings = {"foo": "bar"};
-     writeFileSync(settingsFilePath, JSON.stringify(expectedSettings));
+  test("Verify can load settings.json", () => {
+    const serviceName = "Settings";
+    const expectedSettings = { foo: "bar" };
+    writeFileSync(settingsFilePath, JSON.stringify(expectedSettings));
 
-     const actualSettings = helpers.readSettings(serviceName, settingsFilePath);
+    const actualSettings = helpers.readSettings(serviceName, settingsFilePath);
 
-     expect(actualSettings).toEqual(expectedSettings);
-   });
+    expect(actualSettings).toEqual(expectedSettings);
+  });
 
-   test("Verify cannot load settings.json because it does not exist", () => {
-     //eslint-disable-next-line no-console
-     console.log = jest.fn();
-     const serviceName = "Settings";
-     unlinkSync(settingsFilePath);
+  test("Verify cannot load settings.json because it does not exist", () => {
+    //eslint-disable-next-line no-console
+    console.log = jest.fn();
+    const serviceName = "Settings";
+    unlinkSync(settingsFilePath);
 
-     const actualSettings = helpers.readSettings(serviceName, settingsFilePath);
+    const actualSettings = helpers.readSettings(serviceName, settingsFilePath);
 
-     //eslint-disable-next-line no-console
-     expect(console.log).toHaveBeenCalledWith(expect.stringContaining("[Settings] Unable to read the configuration file: ENOENT: no such file or directory"));
-     expect(actualSettings).toBeNull();
-   });
+    //eslint-disable-next-line no-console
+    expect(console.log).toHaveBeenCalledWith(
+      expect.stringContaining("[Settings] Unable to read the configuration file: ENOENT: no such file or directory"),
+    );
+    expect(actualSettings).toBeNull();
+  });
 
-   test("Verify throws if rawConfig empty", () => {
-     const serviceName = "Settings";
-     const expectedSettings = "";
-     writeFileSync(settingsFilePath, expectedSettings);
+  test("Verify throws if rawConfig empty", () => {
+    const serviceName = "Settings";
+    const expectedSettings = "";
+    writeFileSync(settingsFilePath, expectedSettings);
 
-     expect(() => {helpers.readSettings(serviceName, settingsFilePath)}).toThrow(Error);
-   });
+    expect(() => {
+      helpers.readSettings(serviceName, settingsFilePath);
+    }).toThrow(Error);
+  });
 
-   test("Verify throws with message if rawConfig empty", () => {
-     const serviceName = "Settings";
-     const expectedSettings = "";
-     writeFileSync(settingsFilePath, expectedSettings);
+  test("Verify throws with message if rawConfig empty", () => {
+    const serviceName = "Settings";
+    const expectedSettings = "";
+    writeFileSync(settingsFilePath, expectedSettings);
 
-     try {
-       helpers.readSettings(serviceName, settingsFilePath);
-     } catch (error) {
-       expect(error.message).toBe(`[${serviceName}] Unable to load configuration file ${settingsFilePath}.`);
-     }
-   });
+    try {
+      helpers.readSettings(serviceName, settingsFilePath);
+    } catch (error) {
+      expect(error.message).toBe(`[${serviceName}] Unable to load configuration file ${settingsFilePath}.`);
+    }
+  });
 
   test("Verify throws if json invalid", () => {
     const serviceName = "Settings";
     const expectedSettings = {};
     writeFileSync(settingsFilePath, JSON.stringify(expectedSettings));
-    const mockAddListener = jest.spyOn(JSONC, 'parse');
+    const mockAddListener = jest.spyOn(JSONC, "parse");
     mockAddListener.mockImplementation((rawConfig, parseErrors) => {
-        parseErrors.push({
-            error: 1,
-            offset: 2,
-            length: 3,
-        });
-        return {};
+      parseErrors.push({
+        error: 1,
+        offset: 2,
+        length: 3,
+      });
+      return {};
     });
 
-    expect(() => {helpers.readSettings(serviceName, settingsFilePath)}).toThrow(Error);
+    expect(() => {
+      helpers.readSettings(serviceName, settingsFilePath);
+    }).toThrow(Error);
   });
 
   test("Verify throws with message if json invalid", () => {
     const serviceName = "Settings";
     const expectedSettings = {};
     writeFileSync(settingsFilePath, JSON.stringify(expectedSettings));
-    const mockAddListener = jest.spyOn(JSONC, 'parse');
+    const mockAddListener = jest.spyOn(JSONC, "parse");
     const parseError1 = {
       error: 1,
       offset: 2,
@@ -96,21 +102,20 @@ describe("helpers", () => {
       length: 1,
     };
     mockAddListener.mockImplementation((rawConfig, parseErrors) => {
-        parseErrors.push(parseError1);
-        parseErrors.push(parseError2);
-        return {};
+      parseErrors.push(parseError1);
+      parseErrors.push(parseError2);
+      return {};
     });
     //eslint-disable-next-line no-console
     console.log = jest.fn();
 
-     try {
-       helpers.readSettings(serviceName, settingsFilePath);
-     } catch (error) {
-       const parseErrorsAsString = `${JSON.stringify(parseError1)} ${JSON.stringify(parseError2)}`;
-       //eslint-disable-next-line no-console
-       expect(console.log).toHaveBeenCalledWith(expect.stringContaining(`[${serviceName}] ${parseErrorsAsString}`));
-       expect(error.message).toBe(`[${serviceName}] Unable to load configuration file: ${parseErrorsAsString}`);
-     }
+    try {
+      helpers.readSettings(serviceName, settingsFilePath);
+    } catch (error) {
+      const parseErrorsAsString = `${JSON.stringify(parseError1)} ${JSON.stringify(parseError2)}`;
+      //eslint-disable-next-line no-console
+      expect(console.log).toHaveBeenCalledWith(expect.stringContaining(`[${serviceName}] ${parseErrorsAsString}`));
+      expect(error.message).toBe(`[${serviceName}] Unable to load configuration file: ${parseErrorsAsString}`);
+    }
   });
 });
-
